3441780490c2fd34f56fb8cbf2295897a5b23c5d
Jason Walsh <jwalsh@saylent.com>
2018-08-07 12:16:45 -0400

    Update to 1.6

--- format-code.sh
+++ format-code.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 mkdir -p .cache
 cd .cache
-if [ ! -f google-java-format-1.5-all-deps.jar ]
+VERSION=1.6
+if [ ! -f google-java-format-${VERSION}-all-deps.jar ]
 then
-    curl -LJO "https://github.com/google/google-java-format/releases/download/google-java-format-1.5/google-java-format-1.5-all-deps.jar"
-    chmod 755 google-java-format-1.5-all-deps.jar
+    curl -LJO "https://github.com/google/google-java-format/releases/download/google-java-format-${VERSION}/google-java-format-${VERSION}-all-deps.jar"
+    chmod 755 google-java-format-${VERSION}-all-deps.jar
 fi
 cd ..
 
 changed_java_files=$(git diff --cached --name-only --diff-filter=ACMR | grep ".*java$" )
 echo $changed_java_files
-java -jar .cache/google-java-format-1.5-all-deps.jar --replace $changed_java_files
+java -jar .cache/google-java-format-${VERSION}-all-deps.jar --replace $changed_java_files
